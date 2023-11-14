8390a8c68a554b00c9722e05317ff06978e0928c
codevisualisations <codevisualisations@gmail.com>
2023-11-14 12:01:23 +0000

    finished product

--- app/src/main/java/com/fustun/projectmycity/data/CityCategories.kt
+++ app/src/main/java/com/fustun/projectmycity/data/CityCategories.kt
@@ -5,23 +5,23 @@ import com.fustun.projectmycity.models.CityModels
 
 object CityCategories {
     val attractionsMap: HashMap<Int, CityModels.Attractions> = hashMapOf(
-        R.string.britishMuseum to CityModels.Attractions(R.string.britishMuseum, R.string.britishMuseumDescription, R.drawable.asia),
-        R.string.towerOfLondon to CityModels.Attractions(R.string.towerOfLondon, R.string.towerOfLondonDescription, R.drawable.asia),
-        R.string.buckinghamPalace to CityModels.Attractions(R.string.buckinghamPalace, R.string.buckinghamPalaceDescription, R.drawable.asia),
-        R.string.indiaGate to CityModels.Attractions(R.string.indiaGate, R.string.indiaGateDescription, R.drawable.asia),
-        R.string.lotusTemple to CityModels.Attractions(R.string.lotusTemple, R.string.lotusTempleDescription, R.drawable.asia),
-        R.string.qutubMinar to CityModels.Attractions(R.string.qutubMinar, R.string.qutubMinarDescription, R.drawable.asia),
-        R.string.humayunTomb to CityModels.Attractions(R.string.humayunTomb, R.string.humayunTombDescription, R.drawable.asia),
-        R.string.redFort to CityModels.Attractions(R.string.redFort, R.string.redFortDescription, R.drawable.asia),
-        R.string.chandniChowk to CityModels.Attractions(R.string.chandniChowk, R.string.chandniChowkDescription, R.drawable.asia),
-        R.string.akshardhamTemple to CityModels.Attractions(R.string.akshardhamTemple, R.string.akshardhamTempleDescription, R.drawable.asia),
-        R.string.handloomMuseum to CityModels.Attractions(R.string.handloomMuseum, R.string.handloomMuseumDescription, R.drawable.asia),
-        R.string.centralPark to CityModels.Attractions(R.string.centralPark, R.string.centralParkDescription, R.drawable.asia),
-        R.string.timesSquare to CityModels.Attractions(R.string.timesSquare, R.string.timesSquareDescription, R.drawable.asia),
-        R.string.statueOfLiberty to CityModels.Attractions(R.string.statueOfLiberty, R.string.statueOfLibertyDescription, R.drawable.asia),
-        R.string.sydneyOperaHouse to CityModels.Attractions(R.string.sydneyOperaHouse, R.string.sydneyOperaHouseDescription, R.drawable.asia),
-        R.string.tarongaZoo to CityModels.Attractions(R.string.tarongaZoo, R.string.tarongaZooDescription, R.drawable.asia),
-        R.string.royalBotanicGarden to CityModels.Attractions(R.string.royalBotanicGarden, R.string.royalBotanicGardenDescription, R.drawable.asia)
+        R.string.britishMuseum to CityModels.Attractions(R.string.britishMuseum, R.string.britishMuseumDescription, R.drawable.britishmuseum_jpg),
+        R.string.towerOfLondon to CityModels.Attractions(R.string.towerOfLondon, R.string.towerOfLondonDescription, R.drawable.toweroflondon),
+        R.string.buckinghamPalace to CityModels.Attractions(R.string.buckinghamPalace, R.string.buckinghamPalaceDescription, R.drawable.buckinghampalace),
+        R.string.indiaGate to CityModels.Attractions(R.string.indiaGate, R.string.indiaGateDescription, R.drawable.indiagate),
+        R.string.lotusTemple to CityModels.Attractions(R.string.lotusTemple, R.string.lotusTempleDescription, R.drawable.lotustemple),
+        R.string.qutubMinar to CityModels.Attractions(R.string.qutubMinar, R.string.qutubMinarDescription, R.drawable.qutubminar),
+        R.string.humayunTomb to CityModels.Attractions(R.string.humayunTomb, R.string.humayunTombDescription, R.drawable.humayuntomb),
+        R.string.redFort to CityModels.Attractions(R.string.redFort, R.string.redFortDescription, R.drawable.redfort),
+        R.string.chandniChowk to CityModels.Attractions(R.string.chandniChowk, R.string.chandniChowkDescription, R.drawable.chandnichowk_jpg),
+        R.string.akshardhamTemple to CityModels.Attractions(R.string.akshardhamTemple, R.string.akshardhamTempleDescription, R.drawable.akshardhamtemple),
+        R.string.handloomMuseum to CityModels.Attractions(R.string.handloomMuseum, R.string.handloomMuseumDescription, R.drawable.handloommuseum),
+        R.string.centralPark to CityModels.Attractions(R.string.centralPark, R.string.centralParkDescription, R.drawable.centralpark),
+        R.string.timesSquare to CityModels.Attractions(R.string.timesSquare, R.string.timesSquareDescription, R.drawable.timessquare_jpg),
+        R.string.statueOfLiberty to CityModels.Attractions(R.string.statueOfLiberty, R.string.statueOfLibertyDescription, R.drawable.statueofliberty),
+        R.string.sydneyOperaHouse to CityModels.Attractions(R.string.sydneyOperaHouse, R.string.sydneyOperaHouseDescription, R.drawable.sydneyoperahouse),
+        R.string.tarongaZoo to CityModels.Attractions(R.string.tarongaZoo, R.string.tarongaZooDescription, R.drawable.tarongazoo),
+        R.string.royalBotanicGarden to CityModels.Attractions(R.string.royalBotanicGarden, R.string.royalBotanicGardenDescription, R.drawable.royalbotanicgarden_jpg)
     )
     fun getCityContinents() : List<CityModels.Continent>{
         return listOf(
@@ -34,39 +34,39 @@ object CityCategories {
 
     fun getCityRecommendationsLondon() : List<CityModels.Cities>{
         return listOf(
-            CityModels.Cities(R.string.britishMuseum, R.drawable.emea),
-            CityModels.Cities(R.string.buckinghamPalace, R.drawable.amer),
-            CityModels.Cities(R.string.towerOfLondon, R.drawable.asia),
+            CityModels.Cities(R.string.britishMuseum, R.drawable.britishmuseum_jpg),
+            CityModels.Cities(R.string.buckinghamPalace, R.drawable.buckinghampalace),
+            CityModels.Cities(R.string.towerOfLondon, R.drawable.toweroflondon),
         )
     }
 
     fun getCityRecommendationsNewDelhi() : List<CityModels.Cities>{
         return listOf(
-            CityModels.Cities(R.string.indiaGate, R.drawable.emea),
-            CityModels.Cities(R.string.lotusTemple, R.drawable.amer),
-            CityModels.Cities(R.string.qutubMinar, R.drawable.asia),
-            CityModels.Cities(R.string.humayunTomb, R.drawable.amer),
-            CityModels.Cities(R.string.redFort, R.drawable.asia),
-            CityModels.Cities(R.string.chandniChowk, R.drawable.amer),
-            CityModels.Cities(R.string.akshardhamTemple, R.drawable.asia),
-            CityModels.Cities(R.string.handloomMuseum, R.drawable.amer),
+            CityModels.Cities(R.string.indiaGate, R.drawable.indiagate),
+            CityModels.Cities(R.string.lotusTemple, R.drawable.lotustemple),
+            CityModels.Cities(R.string.qutubMinar, R.drawable.qutubminar),
+            CityModels.Cities(R.string.humayunTomb, R.drawable.humayuntomb),
+            CityModels.Cities(R.string.redFort, R.drawable.redfort),
+            CityModels.Cities(R.string.chandniChowk, R.drawable.chandnichowk_jpg),
+            CityModels.Cities(R.string.akshardhamTemple, R.drawable.akshardhamtemple),
+            CityModels.Cities(R.string.handloomMuseum, R.drawable.handloommuseum),
 
         )
     }
 
     fun getCityRecommendationsNewYork() : List<CityModels.Cities>{
         return listOf(
-            CityModels.Cities(R.string.centralPark, R.drawable.emea),
-            CityModels.Cities(R.string.statueOfLiberty, R.drawable.amer),
-            CityModels.Cities(R.string.timesSquare, R.drawable.asia),
+            CityModels.Cities(R.string.centralPark, R.drawable.centralpark),
+            CityModels.Cities(R.string.statueOfLiberty, R.drawable.statueofliberty),
+            CityModels.Cities(R.string.timesSquare, R.drawable.timessquare_jpg),
         )
     }
 
     fun getCityRecommendationsSydney() : List<CityModels.Cities>{
         return listOf(
-            CityModels.Cities(R.string.sydneyOperaHouse, R.drawable.emea),
-            CityModels.Cities(R.string.royalBotanicGarden, R.drawable.amer),
-            CityModels.Cities(R.string.tarongaZoo, R.drawable.asia),
+            CityModels.Cities(R.string.sydneyOperaHouse, R.drawable.sydneyoperahouse),
+            CityModels.Cities(R.string.royalBotanicGarden, R.drawable.royalbotanicgarden_jpg),
+            CityModels.Cities(R.string.tarongaZoo, R.drawable.tarongazoo),
         )
     }
 
--- app/src/main/java/com/fustun/projectmycity/ui/theme/CityViewModel.kt
+++ app/src/main/java/com/fustun/projectmycity/ui/theme/CityViewModel.kt
@@ -13,54 +13,51 @@ import kotlinx.coroutines.flow.update
 class CityViewModel : ViewModel() {
     private val _CityUiState = MutableStateFlow(CityUiState())
     val cityUiState : StateFlow<CityUiState> = _CityUiState.asStateFlow()
-        fun updateRecommendationsList(title: Int) {
-            _CityUiState.update {
-                when (title) {
-                    R.string.EMEA -> it.copy(
-                        topBarTitle = R.string.LONDON,
-                        cityRecommendationsList = CityCategories.getCityRecommendationsLondon()
-                    )
-                    R.string.AMER -> it.copy(
-                        topBarTitle = R.string.NEW_YORK,
-                        cityRecommendationsList = CityCategories.getCityRecommendationsNewYork()
-                    )
-                    R.string.ASIA -> it.copy(
-                        topBarTitle = R.string.NEW_DELHI,
-                        cityRecommendationsList = CityCategories.getCityRecommendationsNewDelhi()
-                    )
-                    R.string.OCEANIA -> it.copy(
-                        topBarTitle = R.string.SYDNEY,
-                        cityRecommendationsList = CityCategories.getCityRecommendationsSydney()
-                    )
-                    else -> it
-                }
-            }
-        }
-
-        fun updateCurrentAttraction(title: Int){
-            _CityUiState.update {
-                it.copy(
-                    cityAttraction = CityCategories.getCityAttraction(title)
+    fun updateRecommendationsList(title: Int) {
+        _CityUiState.update {
+            when (title) {
+                R.string.EMEA -> it.copy(
+                    topBarTitle = R.string.LONDON,
+                    cityRecommendationsList = CityCategories.getCityRecommendationsLondon()
                 )
-            }
-        }
-
-        fun updateTopBarTitle(title : Int){
-            _CityUiState.update {
-                it.copy(
-                    topBarTitle = title
+                R.string.AMER -> it.copy(
+                    topBarTitle = R.string.NEW_YORK,
+                    cityRecommendationsList = CityCategories.getCityRecommendationsNewYork()
                 )
-            }
-        }
-
-        fun updatePreviousTopBarTitle(title:Int){
-            _CityUiState.update {
-                it.copy(
-                    previousTopBarTitle = title
+                R.string.ASIA -> it.copy(
+                    topBarTitle = R.string.NEW_DELHI,
+                    cityRecommendationsList = CityCategories.getCityRecommendationsNewDelhi()
                 )
+                R.string.OCEANIA -> it.copy(
+                    topBarTitle = R.string.SYDNEY,
+                    cityRecommendationsList = CityCategories.getCityRecommendationsSydney()
+                )
+                else -> it
             }
         }
-
-
-
     }
+
+    fun updateCurrentAttraction(title: Int){
+        _CityUiState.update {
+            it.copy(
+                cityAttraction = CityCategories.getCityAttraction(title)
+            )
+        }
+    }
+
+    fun updateTopBarTitle(title : Int){
+        _CityUiState.update {
+            it.copy(
+                topBarTitle = title
+            )
+        }
+    }
+
+    fun updatePreviousTopBarTitle(title:Int){
+        _CityUiState.update {
+            it.copy(
+                previousTopBarTitle = title
+            )
+        }
+    }
+}
